9646718eac006f7a5947891d2ded9c6d103115a5
Akshara266 <sanagalaakshara@gmail.com>
2025-03-26 20:37:52 -0500

    added files

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const computationRouter = require('./routes/computation');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
